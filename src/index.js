43314b61ec5c068543ed08289ddf209540e9c2f3
Nazar Kruk <nazarkruk@icloud.com>
2022-08-05 00:45:30 -0400

    Added Blog Post and About Author blocks

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-const BASE_URL = 'https://techcrunch.com/wp-json/wp/v2/posts?per_page=4&context=embed'
+const BASE_URL = 'https://techcrunch.com/wp-json/wp/v2/posts'
 
 window.addEventListener('DOMContentLoaded', ()=> {
     getPosts()
 })
+const mainContainer = document.getElementById('main-container')
+const allPostsAria = document.getElementById('show-posts')
+
 
 function getPosts (){
-    const allPostsAria = document.getElementById('show-posts')
-    fetch(BASE_URL)
+    //const allPostsAria = document.getElementById('show-posts')
+    fetch(BASE_URL + '?per_page=4&context=embed')
     .then(res => res.json())
     .then(data => {
         data.forEach(post => {
@@ -15,26 +18,19 @@ function getPosts (){
             <div class="all-posts-article-image-wrapper">
                 <img src= ${post.jetpack_featured_media_url} alt="" class="article-image">
             </div>
-
             <div class="article-data-container">
-
                 <div class="article-data">
                     <span>${post.date.slice(0,10)}</span>
                     <span class="article-data-spacer"></span>
-                    <span>${post.parsely.meta.author[0].name}</span>
-                    
+                    <span>${post.parsely.meta.author[0].name}</span>                  
                 </div>
-
                 <h3 class="title article-title">${post.title.rendered}</h3>
                 <p class="article-description">${post.excerpt.rendered}</p>
-
-                
-                
-
             </div>
+            </div>
+            
         </a>
        `
-
         })
         addReadFullPost()
     }) 
@@ -50,6 +46,74 @@ const addReadFullPost = () => {
 
 
 const displayPost = (event) => {
-    console.log(event.target)
+    fetch (BASE_URL +'/' + `${event.target.parentElement.parentElement.dataset.id}`)
+    .then(res => res.json())
+    .then(post => {
+        mainContainer.innerHTML = `
+        <section class="blog-post section-header-offset">
+        <div class="blog-post-container container">
+            <div class="blog-post-data">
+                <h3 class="title blog-post-title">${post.title.rendered}</h3>
+                <div class="article-data">
+                    <span>${post.date.slice(0,10)}</span>
+                    <span class="article-data-spacer"></span>
+                    <span>${post.parsely.meta.author[0].name}</span>
+                </div>
+                <img src=${post.jetpack_featured_media_url} alt="">
+            </div>
+
+            <div class="container">
+                <p>
+                ${post.content.rendered}
+                </p>
+                
+
+
+                <blockquote class="quote">
+                    <p><span><i class="ri-double-quotes-l"></i></span> Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia voluptates, laboriosam voluptatum quos non consequuntur nesciunt necessitatibus tempora quod inventore corporis rem nihil itaque, at provident minus aliquam veritatis. Labore?  <span><i class="ri-double-quotes-r"></i></span></p>
+                </blockquote>
+            
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis eius possimus hic eligendi distinctio rerum incidunt, esse quasi eum molestiae ducimus ipsam quae, aliquid ullam placeat dolorum nulla vero. Quam? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero quod necessitatibus, aspernatur pariatur asperiores earum quas adipisci veritatis quidem facilis! Nihil veniam quaerat nulla possimus, asperiores vero voluptatum placeat. Eveniet!
+                </p>
+                <div class="author d-grid" id ='author-about' >
+                
+
+                </div>
+            </div>
+        </div>
+    </section>
+        `
+        fetch(`${post._links.author[0].href}`)
+        .then (res => res.json())
+        .then (author =>{
+            //const authorImgUrl = `${author.avatar_urls['96']}`
+            const authorAboutSection = document.getElementById('author-about')
+            authorAboutSection.innerHTML = `
+            
+            <div class="author-image-box">
+                <img src='${author.avatar_urls['96']}' alt="" class="article-image">
+            </div>
+            <div class="author-about" >
+                <h3 class="author-name">${author.name}</h3>
+                <p>${author.cbDescription}</p>
+                <ul class="list social-media">
+                    <li class="list-item">
+                        <a href="#" class="list-link"><i class="ri-instagram-line"></i></a>
+                    </li>
+                    <li class="list-item">
+                        <a href="#" class="list-link"><i class="ri-facebook-circle-line"></i></a>
+                    </li>
+                    <li class="list-item">
+                        <a href="#" class="list-link"><i class="ri-twitter-line"></i></a>
+                    </li>
+                </ul>
+            </div>
+            
+            `
+            
+        })
+    })
 }
+
 
